08b5d1383280566051ece7e7247f92aed43c9985
kvokka <kvokka@yahoo.com>
2021-07-05 20:42:42 +0100

    Add 5th July 2021 as a bank holiday in US

--- lib/ext/business_day.rb
+++ lib/ext/business_day.rb
@@ -3,9 +3,13 @@
 require "business_time"
 require "holidays"
 
-Holidays.between(2.years.ago, Date.today, :us).each do |holiday|
+Holidays.between(2.years.ago, Date.today + 1, :us).each do |holiday|
   BusinessTime::Config.holidays << holiday[:date]
 end
+
+# Some dates might be manually moved by government, so we should update the the
+# settings accordingly
+BusinessTime::Config.holidays << Date.new(2021, 0o7, 0o5)
 
 BusinessTime::Config.beginning_of_workday = BusinessTime::ParsedTime.parse("9:30 am")
 BusinessTime::Config.end_of_workday = BusinessTime::ParsedTime.parse("4:00 pm")
